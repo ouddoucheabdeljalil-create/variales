1b01c3bd8295f718a9b5545a7614ae771ffc3bf1
unknown <ouddoucheabdeljalil@gmail.com>
2026-09-17 15:40:41 +0200

    Moyenne pondérée de trois nombres

---
+++ challange7.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+let num1= Number(prompt("enter first number  "));
+let num2= Number(prompt("enter second number  "));
+let num3= Number(prompt("enter third number  "));
+
+let sum1 = num1 * 2 ;
+let sum2 = num2 * 3 ;
+let sum3 = num3 * 5 ;
+ console.log((sum1 + sum2 + sum3)/10);
