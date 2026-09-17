8dd9828798fd39340e0fb1e5f16675f7261188ea
unknown <ouddoucheabdeljalil@gmail.com>
2026-09-17 15:20:43 +0200

    Calcul et affichage des résultats

---
+++ challange6.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+let Num1 = Number(prompt("enter the first number "));
+let Num2 = Number(prompt("enter the second nember"));
+
+console.log(Num1 + Num2);
+console.log(Num1 - Num2);
+console.log(Num1 * Num2);
+console.log(Num1 / Num2);
