2da974caac5991c03d432343ebcaabfcefe6632d
unknown <ouddoucheabdeljalil@gmail.com>
2026-09-17 13:51:07 +0200

    Affichage Température

---
+++ challange5.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+let C = Number(prompt("enter the temperature : "));
+if (C < 0)
+{
+    console.log("solide");
+}
+else if(C <= 100)
+{
+    console.log("liquide");
+}
+else if ( C >= 100)
+{
+    console.log("Gaz");
+}
+
